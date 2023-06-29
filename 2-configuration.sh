0cc30201beac5cbb5d252ac968fefa9e7eb5a7fc
morrisar <89943445+morrisar@users.noreply.github.com>
2023-06-29 18:43:32 -0400

    Update 2-configuration.sh

--- 2-configuration.sh
+++ 2-configuration.sh
@@ -33,7 +33,7 @@ pacman -Syy
 # ------------------------------------------------------
 # Install Packages
 # ------------------------------------------------------
-pacman --noconfirm -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers bluez bluez-utils cups hplip alsa-utils pipewire pipewire-also pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call tlp sof-firmware acpid os-prober ntfs-3g nvidia nvidia-utils nvidia-settings man git neovim
+pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers bluez bluez-utils cups hplip alsa-utils pipewire pipewire-also pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call tlp sof-firmware acpid os-prober ntfs-3g nvidia nvidia-utils nvidia-settings man git neovim
 
 # ------------------------------------------------------
 # set lang utf8 US
