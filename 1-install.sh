3142b7dcf2e41d612de6bf005da79afb845d9eab
Drew <89943445+morrisar@users.noreply.github.com>
2023-06-29 16:17:33 -0400

    Added Scripts

---
+++ 1-install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+clear
+echo "Arch Install Script"
+echo "by Stephan Raabe (2023)"
+echo "Edited by big dick Morris (Still 2023)"
+echo "-----------------------------------------------------"
+echo ""
+echo "Important: Please make sure that you have followed the "
+echo "manual steps in the README to partition the harddisc!"
+echo "Warning: Run this script at your own risk."
+echo ""
+
+# ------------------------------------------------------
+# Enter partition names
+# ------------------------------------------------------
+lsblk
+read -p "Enter the name of the EFI partition (eg. sda1): " sda1
+read -p "Enter the name of the ROOT partition (eg. sda2): " sda2
+# read -p "Enter the name of the VM partition (keep it empty if not required): " sda3
+
+# ------------------------------------------------------
+# Sync time
+# ------------------------------------------------------
+timedatectl set-ntp true
+timedatectl set-timezone America/New_York 
+
+# ------------------------------------------------------
+# Format partitions
+# ------------------------------------------------------
+mkfs.fat -F 32 /dev/$sda1;
+mkfs.btrfs -f /dev/$sda2
+
+# ------------------------------------------------------
+# Mount points for btrfs
+# ------------------------------------------------------
+mount /dev/$sda2 /mnt
+cd /mnt || return
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@var
+umount /mnt
+
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ /dev/$sda2 /mnt
+mkdir -p /mnt/{boot/efi,home,var}
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@home /dev/$sda2 /mnt/home
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@var /dev/$sda2 /mnt/var
+mount /dev/$sda1 /mnt/boot/efi
+
+# ------------------------------------------------------
+# Select the fastest mirrors for location
+# ------------------------------------------------------
+reflector --country "United States" --age 6 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Sy
+
+# ------------------------------------------------------
+# Install base packages
+# ------------------------------------------------------
+pacstrap -K /mnt base git linux linux-firmware neovim intel-ucode btrfs-progs reflector
+
+# ------------------------------------------------------
+# Generate fstab
+# ------------------------------------------------------
+genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab
+
+# ------------------------------------------------------
+# Install configuration scripts
+# ------------------------------------------------------
+mkdir /mnt/archinstall
+cp 2-configuration.sh /mnt/archinstall/
+cp 3-yay.sh /mnt/archinstall/
+cp 4-zram.sh /mnt/archinstall/
+cp 5-timeshift.sh /mnt/archinstall/
+cp 6-preload.sh /mnt/archinstall/
+cp snapshot.sh /mnt/archinstall/
+
+# ------------------------------------------------------
+# Chroot to installed sytem
+# ------------------------------------------------------
+arch-chroot /mnt ./archinstall/2-configuration.sh
